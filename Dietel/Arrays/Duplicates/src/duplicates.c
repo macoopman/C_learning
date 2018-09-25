db6e63c6b3d66baf9515b95a3759dd60b50b79c9
Mike Coopman <macoopman@gmail.com>
2018-09-25 15:56:23 -0700

    duplicates first commit

---
+++ Dietel/Arrays/Duplicates/src/duplicates.c
@@ -0,0 +1,14 @@
+// Program reads in 20 numbers (10 - 100). As each number is read, print it only
+// if it is not a duplicateof a number alread read.
+// Provide for the "worst case" in which all 20 numbers are different.
+// Use the smallest possible array to solve this problem
+
+
+#include <stdio.h>
+
+
+int main(void){
+
+
+    
+}
