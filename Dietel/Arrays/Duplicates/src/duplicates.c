44979448eac276ac230a2c5b5b5ef03b0eea484e
Mike Coopman <macoopman@gmail.com>
2018-09-25 16:13:39 -0700

    basic framework for duplicates added loop, valid fuction, print function

--- Dietel/Arrays/Duplicates/src/duplicates.c
+++ Dietel/Arrays/Duplicates/src/duplicates.c
@@ -5,10 +5,50 @@
 
 
 #include <stdio.h>
+#define SIZE 5
 
+int valid(int a[], int size, int input);
+void printArray(int a[], int size);
 
 int main(void){
 
+    int inputArray[SIZE] = {0};
+    int inputArrayCount = 0;
+    int inputCount = 0;
+    int userInput = 0;
 
-    
+
+
+
+    do{
+        printf("\n\nEnter Number: ");
+        scanf("%d", &userInput);
+        ++inputCount;
+
+        if(valid(inputArray, SIZE, userInput)){
+            inputArray[inputArrayCount] = userInput;
+            ++inputArrayCount;
+        }
+    } while(inputCount <= SIZE);
+
+    printArray(inputArray, inputArrayCount);
+
+
+}
+
+int valid(int a[], int size, int input){
+    int isValid = 1;
+    for(int i = 0; i < size; ++i){
+        if(a[i] == input){
+            isValid = 0;
+        }
+    }
+    return isValid;
+}
+
+void printArray(int a[], int size){
+    for(int i = 0; i < size; ++i){
+        printf("%5d", a[i]);
+        }
+    puts("");
 }
