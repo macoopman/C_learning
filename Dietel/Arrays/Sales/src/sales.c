e3c9fc1ef21ecfc5b232f4d93e5779e1c122b916
Mike Coopman <macoopman@gmail.com>
2018-09-26 19:33:25 -0700

    added salesTotal function

--- Dietel/Arrays/Sales/src/sales.c
+++ Dietel/Arrays/Sales/src/sales.c
@@ -9,53 +9,64 @@
 #define SALESPERSON 4
 #define PRODUCTS 5
 
-void printSales(double a[][PRODUCTS], int size);
-void createSlip(double a[][PRODUCTS], int employee_number, int product_number, int value);
-void autoDataFill(double a[SALESPERSON][PRODUCTS], int min, int max, int perDay, int days);
+void printSales(double a[][SALESPERSON], int size);
+void createSlip(double a[][SALESPERSON], int employee_number, int product_number, int value);
+void autoDataFill(double a[PRODUCTS][SALESPERSON], int min, int max, int perDay, int days);
+double salesTotal(double a[][SALESPERSON], int person);
+
 
 
 int main(void){
     srand(time(NULL));
 
-    double sales[SALESPERSON][PRODUCTS] = {0};
+    double sales[PRODUCTS][SALESPERSON] = {0};
 
     autoDataFill(sales, 1, 100, 5, 30);
 
-    printSales(sales, SALESPERSON);
+    printSales(sales, PRODUCTS);
 
     puts("\n\n\n");
 
-    createSlip(sales, 1, 3,50.25);
 
-    printSales(sales, SALESPERSON);
-
-
+    printf("%.2f\n", salesTotal(sales, 1));
 
 
 
 }
 
 
-void createSlip(double a[][PRODUCTS], int employee_number, int product_number, int value){
+void createSlip(double a[][SALESPERSON], int employee_number, int product_number, int value){
     a[employee_number - 1][product_number - 1] += value;
 }
 
+double salesTotal(double a[][SALESPERSON], int person){
+    double total = 0;
+    for(int i = 0; i < PRODUCTS; ++i){
+        total += a[i][person - 1];
+    }
+    return total;
+}
 
-void printSales(double a[][PRODUCTS], int size){
+
+void printSales(double a[][SALESPERSON], int size){
+    printf("%21s%9s%9s%9s\n\n", "Person 1", "Person 2","Person 3","Person 4");
     for(int i = 0; i < size ; ++i){
-        for(int j = 0; j < PRODUCTS; ++j){
+        printf("Product %d:   ", i +1);
+        for(int j = 0; j < SALESPERSON; ++j){
+
             printf("%4.2f  ", a[i][j]);
         }
         puts("");
     }
+    puts("");
 }
 
-void autoDataFill(double a[SALESPERSON][PRODUCTS], int min, int max, int perDay, int days){
+void autoDataFill(double a[PRODUCTS][SALESPERSON], int min, int max, int perDay, int days){
 
     for(int d = 0; d < days; ++d){
         for(int pDay = 0; pDay < perDay; ++pDay){
-            for(int i = 0; i < SALESPERSON ; ++i){
-                for(int j = 0; j < PRODUCTS; ++j){
+            for(int i = 0; i < PRODUCTS ; ++i){
+                for(int j = 0; j < SALESPERSON; ++j){
                     a[i][j] += (double)(rand() % max + min);
                 }
             }
