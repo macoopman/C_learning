f11e1ddfd336f141581677e120ad11ad2ebc8a52
Mike Coopman <macoopman@gmail.com>
2018-09-26 17:38:01 -0700

    added dummy data function

--- Dietel/Arrays/Sales/src/sales.c
+++ Dietel/Arrays/Sales/src/sales.c
@@ -3,17 +3,41 @@
 
 
 #include <stdio.h>
-#define SALES_PERSON 4
+#include <time.h>
+#include <stdlib.h>
+
+#define SALESPERSON 4
 #define PRODUCTS 5
 
 void printSales(double a[][PRODUCTS], int size);
+void createSlip(double a[][PRODUCTS], int employee_number, int product_number, int value);
+void autoDataFill(double a[SALESPERSON][PRODUCTS], int min, int max);
 
 
 int main(void){
+    srand(time(NULL));
 
-    double sales[SALES_PERSON][PRODUCTS] = {0};
+    double sales[SALESPERSON][PRODUCTS] = {0};
 
-    printSales(sales, SALES_PERSON);
+    autoDataFill(sales, 1, 100);
+
+    printSales(sales, SALESPERSON);
+
+    puts("\n\n\n");
+
+    createSlip(sales, 1, 3,50.25);
+
+    printSales(sales, SALESPERSON);
+
+
+
+
+
+}
+
+
+void createSlip(double a[][PRODUCTS], int employee_number, int product_number, int value){
+    a[employee_number - 1][product_number - 1] += value;
 }
 
 
@@ -23,5 +47,13 @@ void printSales(double a[][PRODUCTS], int size){
             printf("%4.2f  ", a[i][j]);
         }
         puts("");
+    }
+}
+
+void autoDataFill(double a[SALESPERSON][PRODUCTS], int min, int max){
+    for(int i = 0; i < SALESPERSON ; ++i){
+        for(int j = 0; j < PRODUCTS; ++j){
+            a[i][j] = (double)(rand() % max + min);
+        }
     }
 }
