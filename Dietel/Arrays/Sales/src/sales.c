e479df60222baef78384b500ac7fb89aa2fa8f09
Mike Coopman <macoopman@gmail.com>
2018-09-26 17:19:45 -0700

    added printSales function

--- Dietel/Arrays/Sales/src/sales.c
+++ Dietel/Arrays/Sales/src/sales.c
@@ -6,10 +6,22 @@
 #define SALES_PERSON 4
 #define PRODUCTS 5
 
+void printSales(double a[][PRODUCTS], int size);
+
 
 int main(void){
 
     double sales[SALES_PERSON][PRODUCTS] = {0};
 
+    printSales(sales, SALES_PERSON);
+}
 
+
+void printSales(double a[][PRODUCTS], int size){
+    for(int i = 0; i < size ; ++i){
+        for(int j = 0; j < PRODUCTS; ++j){
+            printf("%4.2f  ", a[i][j]);
+        }
+        puts("");
+    }
 }
