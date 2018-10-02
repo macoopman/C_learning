1761048d7f205b04c489c2e9e0237ef441906bd8
Mike Coopman <macoopman@gmail.com>
2018-10-02 15:05:47 -0700

    recursive implementation of a print array function

---
+++ Dietel/Arrays/PrintArray_Recursive/src/print.c
@@ -0,0 +1,21 @@
+// recursive print function
+
+
+#include <stdio.h>
+#define SIZE 10
+
+void print(int a[], int size){
+    if(size != 0){
+
+        print(a, size - 1);
+        printf("%d ", a[size-1]);
+
+    }
+}
+
+int main(void){
+    int a[SIZE] = {1,2,3,4,5,6,7,8,9,10};
+    print(a,SIZE);
+    puts("");
+
+}
