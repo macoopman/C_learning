7cb954e643086407f335b16823db6b647b4a8249
Mike Coopman <macoopman@gmail.com>
2018-10-03 21:11:18 -0700

    shuffle first commit

---
+++ Dietel/Pointers/Card_Shuffling/src/shuffle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void shuffle(int wDeck[][13]);
+void deal(const int wDeck[][13], const char *wFace[], const char *wSuit[]);
+
+int main(void){
+
+    
+}
